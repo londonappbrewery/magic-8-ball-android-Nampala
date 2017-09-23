5582bda00ad3541610ecfbf84d56e4763ebfb730
Nampala <charles.isabirye@gmail.com>
2017-09-23 21:18:47 +0200

    I just cant figure out what ive done wrong. The app refused to launch in the emulator. I cant even see the results in the Android monitor. It says app cannot launch.

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -2,6 +2,16 @@ package com.londonappbrewery.magiceightball;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
+
+import static com.londonappbrewery.magiceightball.R.drawable.ball2;
+import static com.londonappbrewery.magiceightball.R.drawable.ball3;
+import static com.londonappbrewery.magiceightball.R.drawable.ball4;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +19,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button shakeButton = (Button) findViewById(R.id.shake_button);
+        final ImageView image8ball = (ImageView) findViewById(R.id.image_8Ball);
+
+        final int[] ballArray = {R.drawable.ball1,R.drawable.ball2, R.drawable.ball3, R.drawable.ball4, R.drawable.ball5};
+
+        shakeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("magiceightball", "Heck yeah!");
+
+                Random numberToChangeBall = new Random();
+                int thisNumber = numberToChangeBall.nextInt(5);
+                Log.d("magiceightball", "The random number is: " + thisNumber);
+                image8ball.setImageResource(ballArray[thisNumber]);
+            }
+
+        });
+
     }
+
 }
